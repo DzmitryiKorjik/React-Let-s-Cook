51c14c31bf0d11c1e124c4b53b07a60620d768f7
Dzmitryi <dmardovitch@gmail.com>
2025-01-21 09:48:09 +0100

    first commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+      <p>This is a React application.</p>
+
+    </main>
+  );
+}
+
+export default App;
